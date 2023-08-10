00510ff20fb78ef203ea8fe2f77ccbb990852424
Digbijaya Lenka <digbijayalenka@gmail.com>
2023-08-11 02:12:26 +0530

    ,

---
+++ Day-2/coin.py
@@ -0,0 +1,7 @@
+import random
+
+random_number = random.randint(1,2)
+if random_number == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ Day-2/test.py
@@ -0,0 +1,7 @@
+import random
+
+random_number = random.randint(1,100)
+# print(random_number)
+
+random_decimal = random.random()*5
+print(random_decimal)
