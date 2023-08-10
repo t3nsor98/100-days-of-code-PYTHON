ffc648bd3807bb6ba2a0a64bf12b2c11b7945cf7
Digbijaya Lenka <digbijayalenka@gmail.com>
2023-08-11 05:22:19 +0530

    .

---
+++ Day-3/highest.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest_mark = 0;
+
+for mark in student_scores:
+    if mark > highest_mark:
+        highest_mark = mark
+print(f"The highest score in the class is: {highest_mark}")  
+
+
+
+
