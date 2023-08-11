33d0cee1bac5524f380643b736f4e91df184756a
Digbijaya Lenka <digbijayalenka@gmail.com>
2023-08-11 05:51:59 +0530

    Create password.py

---
+++ Day-3/password.py
@@ -0,0 +1,23 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+for n in range(0,nr_letters):
+    password.append(letters[random.randint(0,(len(letters)-1))])
+for n in range(0,nr_symbols):
+    password.append(symbols[random.randint(0,(len(symbols)-1))])
+for n in range(0,nr_numbers):
+    password.append(numbers[random.randint(0,(len(numbers)-1))])
+    
+# print("".join(password))
+
+new_password = random.sample(password, len(password))
+print("".join(new_password))
