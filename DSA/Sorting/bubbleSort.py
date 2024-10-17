85c32186ea8f71f4b0b2f48d8a91523681c6414b
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-10-18 01:37:29 +0530

    bubble sort

--- DSA/Sorting/bubbleSort.py
+++ DSA/Sorting/bubbleSort.py
@@ -1,5 +1,12 @@
 my_array = [64, 34, 25, 12, 22, 11, 90, 5]
 
-len_of_array = 
+len_of_array = len(my_array)
+# print(len_of_array)
 
-for i in range()
+for i in range(len_of_array):
+    for j in range(len_of_array - i - 1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j],my_array[j+1] = my_array[j+1],my_array[j]
+            
+            
+print(F"Sorted Array: {my_array}")
